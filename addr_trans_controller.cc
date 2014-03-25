b8d84534860c331ed6eb759f9657d51b3ad4026b
Jinglei Ren <jinglei.ren@gmail.com>
2014-03-25 13:59:14 -0400

    Make AddrTransController core functions virtual.

--- addr_trans_controller.cc
+++ addr_trans_controller.cc
@@ -2,36 +2,6 @@
 // Copyright (c) 2014 Jinglei Ren <jinglei@ren.systems>
 
 #include "addr_trans_controller.h"
-#include <iostream> //DEBUG
-
-// Space partition (low -> high):
-// DRAM direct |(DRAM size)|| NVM direct |(phy_limit)||
-// ATT buffer || DRAM backup || Page table buffer 
-AddrTransController::AddrTransController(uint64_t dram_size, uint64_t nvm_size, 
-    AddrTransTable& blk_tbl, AddrTransTable& pg_tbl, MemStore* mem_store): 
-    
-    dram_size_(dram_size), nvm_size_(nvm_size), 
-    block_table_(blk_tbl), page_table_(pg_tbl),
-    phy_limit_(nvm_size - blk_tbl.image_size() - pg_tbl.image_size()),
-    mem_store_(mem_store) {
- 
-  assert(phy_limit_ >= dram_size_); 
-  block_table_.set_image_floor(phy_limit());
-  page_table_.set_image_floor(dram_size_ + nvm_size_ - pg_tbl.image_size()); 
-  assert(block_table_.image_floor() + block_table_.image_size() ==
-      page_table_.image_floor() - dram_size_); 
-}
-
-AddrTransController::AddrTransController(uint64_t phy_range, 
-    AddrTransTable& blk_tbl, AddrTransTable& pg_tbl, uint64_t dram_size,
-    MemStore* mem_store):
-
-    AddrTransController(dram_size,
-        phy_range + blk_tbl.image_size() + pg_tbl.image_size(),
-        blk_tbl, pg_tbl, mem_store) {
- 
-  assert(phy_limit() == phy_range); 
-}
 
 uint64_t AddrTransController::LoadAddr(uint64_t phy_addr) {
   assert(phy_addr < phy_limit());
@@ -77,3 +47,9 @@ bool AddrTransController::Probe(uint64_t phy_addr) {
     return block_table_.Probe(phy_addr);
   }
 }
+
+void AddrTransController::NewEpoch() {
+  page_table_.NewEpoch();
+  block_table_.NewEpoch();
+}
+
--- addr_trans_controller.h
+++ addr_trans_controller.h
@@ -14,11 +14,12 @@ class AddrTransController {
     AddrTransTable& blk_tbl, AddrTransTable& pg_tbl, MemStore* mem_store);
   AddrTransController(uint64_t phy_range, AddrTransTable& blk_tbl,
     AddrTransTable& pg_tbl, uint64_t dram_size, MemStore* mem_store);
+  virtual ~AddrTransController() { }
 
-  uint64_t LoadAddr(uint64_t phy_addr);
-  uint64_t StoreAddr(uint64_t phy_addr);
-  bool Probe(uint64_t phy_addr);
-  void NewEpoch();
+  virtual uint64_t LoadAddr(uint64_t phy_addr);
+  virtual uint64_t StoreAddr(uint64_t phy_addr);
+  virtual bool Probe(uint64_t phy_addr);
+  virtual void NewEpoch();
 
   int cache_block_size() const { return block_table_.block_size(); }
   uint64_t phy_limit() const { return phy_limit_; }
@@ -26,18 +27,44 @@ class AddrTransController {
   uint64_t nvm_size() const { return nvm_size_; }
   MemStore* mem_store() const { return mem_store_; }
   void set_mem_store(MemStore* mem_store) { mem_store_ = mem_store; }
+ protected:
+  AddrTransTable& block_table_;
+  AddrTransTable& page_table_; 
  private:
   const uint64_t dram_size_;
   const uint64_t nvm_size_;
-  AddrTransTable& block_table_;
-  AddrTransTable& page_table_;
   const uint64_t phy_limit_;
   MemStore* mem_store_;
 };
 
-inline void AddrTransController::NewEpoch() {
-  page_table_.NewEpoch();
-  block_table_.NewEpoch();
+// Space partition (low -> high):
+// DRAM direct |(DRAM size)|| NVM direct |(phy_limit)||
+// ATT buffer || DRAM backup || Page table buffer 
+inline AddrTransController::AddrTransController(
+    uint64_t dram_size, uint64_t nvm_size,
+    AddrTransTable& blk_tbl, AddrTransTable& pg_tbl, MemStore* mem_store):
+
+    dram_size_(dram_size), nvm_size_(nvm_size),
+    block_table_(blk_tbl), page_table_(pg_tbl),
+    phy_limit_(nvm_size - blk_tbl.image_size() - pg_tbl.image_size()),
+    mem_store_(mem_store) {
+
+  assert(phy_limit_ >= dram_size_);
+  block_table_.set_image_floor(phy_limit());
+  page_table_.set_image_floor(dram_size_ + nvm_size_ - pg_tbl.image_size());
+  assert(block_table_.image_floor() + block_table_.image_size() ==
+      page_table_.image_floor() - dram_size_);
+}
+
+inline AddrTransController::AddrTransController(uint64_t phy_range,
+    AddrTransTable& blk_tbl, AddrTransTable& pg_tbl, uint64_t dram_size,
+    MemStore* mem_store):
+
+    AddrTransController(dram_size,
+        phy_range + blk_tbl.image_size() + pg_tbl.image_size(),
+        blk_tbl, pg_tbl, mem_store) {
+
+  assert(phy_limit() == phy_range);
 }
 
 #endif // SEXAIN_ADDR_TRANS_CONTROLLER_H_
