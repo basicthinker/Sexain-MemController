006ef2053f83bdec756df5acc475e48e242d685c
Jinglei Ren <jinglei.ren@gmail.com>
2014-04-12 13:27:17 -0400

    Simplify AddrTransTable. Move scheme logic out of it.

--- addr_trans_table.cc
+++ addr_trans_table.cc
@@ -3,88 +3,39 @@
 
 #include "addr_trans_table.h"
 
-uint64_t AddrTransTable::LoadAddr(uint64_t phy_addr) {
+uint8_t* AddrTransTable::Lookup(uint64_t phy_addr) {
   uint64_t phy_tag = Tag(phy_addr);
   std::unordered_map<uint64_t, int>::iterator it = tag_index_.find(phy_tag);
   if (it == tag_index_.end()) { // not hit
-    return phy_addr;
+    return NULL;
   } else {
     ATTEntry& entry = entries_[it->second];
-    assert(entry.valid && entry.phy_tag == phy_tag);
-    queues_[entry.dirty].Remove(it->second);
-    queues_[entry.dirty].PushBack(it->second);
-    return Trans(phy_addr, entry.mach_tag);
+    assert(entry.state != FREE_ENTRY && entry.phy_tag == phy_tag);
+    queues_[entry.state].Remove(it->second);
+    queues_[entry.state].PushBack(it->second);
+    return Trans(phy_addr, entry.mach_addr);
   }
 }
 
-enum ATTOperation AddrTransTable::Probe(uint64_t phy_addr) {
-  assert(length() != 0);
-  uint64_t tag = Tag(phy_addr);
-  std::unordered_map<uint64_t, int>::iterator it = tag_index_.find(tag);
-  if (it == tag_index_.end()) { // not hit
-    if (queues_[0].Empty()) return EPOCH;
-    int i = queues_[0].Front(); // clean queue
-    ATTEntry& entry = entries_[i];
+void AddrTransTable::SetupTag(uint64_t phy_tag, uint8_t* mach_addr) {
+  assert(tag_index_.find(phy_tag) == tag_index_.end());
+  assert(!queues_[FREE_ENTRY].Empty());
+  int i = queues_[FREE_ENTRY].PopFront();
+  tag_index_[phy_tag] = i;
+  queues_[DIRTY_ENTRY].PushBack(i);
 
-    if (entry.valid) {
-      return WRBACK;
-    } else {
-      return REGWR; // direct write
-    }
-  } else {
-    ATTEntry& entry = entries_[it->second];
-    assert(entry.valid && entry.phy_tag == tag);
-    if (entry.dirty) {
-      return REGWR; // overwrite
-    } else {
-      return SHRINK;
-    }
-  }
+  entries_[i].phy_tag = phy_tag;
+  entries_[i].mach_addr = mach_addr;
+  entries_[i].state = DIRTY_ENTRY;
 }
 
-uint64_t AddrTransTable::StoreAddr(uint64_t phy_addr) {
-  uint64_t phy_tag = Tag(phy_addr);
-  std::unordered_map<uint64_t, int>::iterator it = tag_index_.find(phy_tag);
-  if (it == tag_index_.end()) { // not hit
-    assert(!queues_[0].Empty());
-    int i = queues_[0].PopFront(); // clean queue
-    ATTEntry& entry = entries_[i];
-
-    if (entry.valid) {
-      mem_store_.OnWriteBack(entry.phy_tag, entry.mach_tag, block_bits_);
-      tag_index_.erase(entry.phy_tag);
-      mapper_.UnmapShadowTag(entry.mach_tag);
-    }
-
-    uint64_t mach_tag = mapper_.MapShadowTag(phy_tag, i);
-    mem_store_.OnNewMapping(phy_tag, mach_tag, block_bits_);
-    if (!entry.valid) mem_store_.OnDirectWrite(phy_tag, mach_tag, block_bits_);
-
-    entries_[i].phy_tag = phy_tag;
-    entries_[i].mach_tag = mach_tag;
-    entries_[i].dirty = true;
-    entries_[i].valid = true;
-
-    queues_[1].PushBack(i); // dirty queue
-    tag_index_[phy_tag] = i;
-    return Trans(phy_addr, mach_tag);
-
-  } else { // hit
-    ATTEntry& entry = entries_[it->second];
-    assert(entry.valid && entry.phy_tag == phy_tag);
-    queues_[entry.dirty].Remove(it->second);
-    if (entry.dirty) {
-      mem_store_.OnOverwrite(entry.phy_tag, entry.mach_tag, block_bits_);
-      queues_[1].PushBack(it->second);
-      return Trans(phy_addr, entry.mach_tag);
-    } else {
-      mem_store_.OnShrink(entry.phy_tag, entry.mach_tag, block_bits_);
-      tag_index_.erase(it);
-      entry.valid = false;
-      queues_[0].PushFront(it->second);
-      mapper_.UnmapShadowTag(entry.mach_tag);
-      return phy_addr;
-    }
+int AddrTransTable::Clean() {
+  int i = queues_[DIRTY_ENTRY].PopFront();
+  while (i != -EINVAL) {
+    queues_[CLEAN_ENTRY].PushBack(i);
+    assert(entries_[i].state == DIRTY_ENTRY);
+    entries_[i].state = CLEAN_ENTRY;
+    i = queues_[DIRTY_ENTRY].PopFront();
   }
 }
 
@@ -92,12 +43,11 @@ void AddrTransTable::RevokeTag(uint64_t tag) {
   std::unordered_map<uint64_t, int>::iterator it = tag_index_.find(tag);
   if (it != tag_index_.end()) {
     ATTEntry& entry = entries_[it->second];
-    assert(entry.valid && entry.phy_tag == tag);
-    assert(!entry.dirty);
-    mem_store_.OnRevoke(entry.phy_tag, entry.mach_tag, block_bits_);
+    assert(entry.state != FREE_ENTRY && entry.phy_tag == tag);
+    queues_[entry.state].Remove(it->second);
+    queues_[FREE_ENTRY].PushBack(it->second);
+    entry.state = FREE_ENTRY;
     tag_index_.erase(it);
-    entry.valid = false;
-    mapper_.UnmapShadowTag(entry.mach_tag);
   }
 }
 
--- addr_trans_table.h
+++ addr_trans_table.h
@@ -8,97 +8,63 @@
 #include <cstdint>
 #include <vector>
 #include <unordered_map>
-#include <iostream>
 #include "index_queue.h"
-#include "shadow_tag_mapper.h"
-#include "mem_store.h"
+
+enum ATTState {
+  DIRTY_ENTRY = 0,
+  CLEAN_ENTRY,
+  FREE_ENTRY,
+};
 
 struct ATTEntry {
   uint64_t phy_tag;
-  uint64_t mach_tag;
+  uint8_t* mach_addr;
   IndexNode queue_node;
-  bool dirty;
-  bool valid;
+  ATTState state;
 
-  ATTEntry() : dirty(false), valid(false) {
+  ATTEntry() : state(FREE_ENTRY) {
     queue_node.first = queue_node.second = -EINVAL;
   }
 };
 
-enum ATTOperation {
-  EPOCH = 0,
-  SHRINK,
-  WRBACK,
-  REGWR,
-};
-
 class AddrTransTable : public IndexArray {
  public:
-  AddrTransTable(int block_bits, ShadowTagMapper& mapper, MemStore& mem);
+  AddrTransTable(int length, int block_bits);
 
-  enum ATTOperation Probe(uint64_t phy_addr);
-  uint64_t LoadAddr(uint64_t phy_addr);
-  uint64_t StoreAddr(uint64_t phy_addr);
-  void NewEpoch();
+  uint8_t* Lookup(uint64_t phy_addr);
+  void SetupTag(uint64_t phy_tag, uint8_t* mach_addr);
+  int Clean();
+  void RevokeTag(uint64_t phy_tag);
 
-  uint64_t Tag(uint64_t addr) const { return addr >> block_bits_; }
-  void RevokeTag(uint64_t tag);
+  bool IsEmpty(ATTState state) { return queues_[state].Empty(); }
 
-  uint64_t image_floor() const;
-  void set_image_floor(uint64_t addr);
-
+  uint64_t Tag(uint64_t addr) { return addr >> block_bits_; }
   int block_size() const { return 1 << block_bits_; }
   int block_bits() const { return block_bits_; }
-  int length() const { return mapper_.length(); }
-  int image_size() const { return block_size() * length(); }
   IndexNode& operator[](int i) { return entries_[i].queue_node; }
 
  private:
   uint64_t Addr(uint64_t tag) const { return tag << block_bits_; }
-  uint64_t Trans(uint64_t phy_addr, uint64_t mach_tag);
+  uint8_t* Trans(uint64_t phy_addr, uint8_t* mach_addr);
 
   const int block_bits_;
   const uint64_t block_mask_;
-  ShadowTagMapper& mapper_;
-  MemStore& mem_store_;
+  const int length_;
   std::unordered_map<uint64_t, int> tag_index_;
   std::vector<ATTEntry> entries_;
   std::vector<IndexQueue> queues_;
 };
 
-inline AddrTransTable::AddrTransTable(int block_bits,
-    ShadowTagMapper& mapper, MemStore& mem) :
-
-    block_bits_(block_bits), block_mask_(block_size() - 1), mapper_(mapper),
-    mem_store_(mem), entries_(length()), queues_(2, *this) {
-
-  for (int i = 0; i < length(); ++i) {
-    queues_[0].PushBack(i); // clean queue
+inline AddrTransTable::AddrTransTable(int length, int block_bits) :
+    length_(length), block_bits_(block_bits), block_mask_(block_size() - 1),
+    entries_(length_), queues_(3, *this) {
+  for (int i = 0; i < length_; ++i) {
+    queues_[FREE_ENTRY].PushBack(i);
   }
 }
 
-inline void AddrTransTable::NewEpoch() {
-  assert(IndexIntersection(queues_[0], queues_[1]).size() == 0);
-  while (!queues_[1].Empty()) {
-    int i = queues_[1].PopFront();
-    entries_[i].dirty = false;
-    queues_[0].PushBack(i);
-  }
-  assert(queues_[0].GetLength() == entries_.size());
-}
-
-inline uint64_t AddrTransTable::image_floor() const {
-  assert(mapper_.floor() >= 0);
-  return Addr(mapper_.floor());
-}
-
-inline void AddrTransTable::set_image_floor(uint64_t addr) {
-  assert((addr & block_mask_) == 0);
-  mapper_.set_floor(Tag(addr));
-}
-
-inline uint64_t AddrTransTable::Trans(uint64_t phy_addr, uint64_t mach_tag) {
-  return Addr(mach_tag) + (phy_addr & block_mask_);
+inline uint8_t* AddrTransTable::Trans(uint64_t phy_addr, uint8_t* mach_addr) {
+  return mach_addr + (phy_addr & block_mask_);
 }
 
 #endif // SEXAIN_ADDR_TRANS_TABLE_H_
