a063f86078ae5647f0988892a57915d6bc335c95
Jinglei Ren <jinglei.ren@gmail.com>
2014-07-10 10:57:25 +0800

    Update CacheController to support monitoring for both ATT and PTT.

--- gem5-stable/src/mem/cache/cache_controller.h
+++ gem5-stable/src/mem/cache/cache_controller.h
@@ -9,6 +9,7 @@
 #include <cassert>
 
 #include "mem/cache/base.hh"
+#include "mem/abstract_mem.hh"
 #include "params/CacheController.hh"
 
 class CacheController : public SimObject {
@@ -19,26 +20,47 @@ class CacheController : public SimObject {
   void DirtyBlock(uint64_t addr, int size);
 
   int block_size() const { return block_size_; }
-  int block_mask() const { return block_mask_; }
+  uint64_t block_mask() const { return block_mask_; }
   int att_length() const { return att_length_; }
+
+  int page_size() const { return page_size_; }
+  uint64_t page_mask() const { return page_mask_; }
+  int ptt_length() const { return ptt_length_; }
 
  private:
 
   void WritebackAll();
 
+  AbstractMemory* memory_;
   std::vector<BaseCache*> caches_;
+
   std::map<uint64_t, int> blocks_;
   const int block_bits_;
   const int att_length_;
-  uint64_t block_size_;
+  int block_size_;
   uint64_t block_mask_;
+
+  std::map<uint64_t, int> pages_;
+  const int page_bits_;
+  const int ptt_length_;
+  int page_size_;
+  uint64_t page_mask_;
 };
 
 inline CacheController::CacheController(const CacheControllerParams* p) :
-    SimObject(p), block_bits_(p->block_bits), att_length_(p->att_length) {
+    SimObject(p), memory_(p->memory),
+    block_bits_(p->block_bits), att_length_(p->att_length),
+    page_bits_(p->page_bits), ptt_length_(p->ptt_length) {
+
+  assert(memory_);
+
   assert(block_bits_ > 0 && att_length_ > 0);
   block_size_ = (1 << block_bits_);
   block_mask_ = block_size_ - 1;
+
+  assert(page_bits_ > 0 && ptt_length_ > 0);
+  page_size_ = (1 << page_bits_);
+  page_mask_ = page_size_ - 1;
 }
 
 inline void CacheController::RegisterCache(BaseCache* const cache) {
@@ -50,10 +72,14 @@ inline void CacheController::DirtyBlock(uint64_t addr, int size) {
   uint64_t begin = addr & ~block_mask();
   uint64_t end = (addr + size - 1) & ~block_mask();
   for (uint64_t a = begin; a <= end; a += block_size()) {
-    ++blocks_[a];
+    if (memory_->isStatic(a)) {
+      ++pages_[a & ~page_mask()];
+    } else {
+      ++blocks_[a];
+    }
   }
 
-  if (blocks_.size() == att_length_) {
+  if (blocks_.size() == att_length_ || pages_.size() == ptt_length_) {
     WritebackAll();
   }
 }
@@ -64,6 +90,7 @@ inline void CacheController::WritebackAll() {
     (*it)->memWritebackTiming();
   }
   blocks_.clear();
+  pages_.clear();
 }
 
 #endif // SEXAIN_CACHE_CONTROLLER_H_
