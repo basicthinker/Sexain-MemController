602bca885147f1854f18562b160ac27558657583
Jinglei Ren <jinglei.ren@gmail.com>
2014-07-23 05:25:01 +0800

    Update Profiler to be byte-based.

--- profiler.h
+++ profiler.h
@@ -13,21 +13,20 @@ class Profiler {
   Profiler(const Profiler& p);
 
   void AddTableOp(int num = 1) { num_table_ops_ += num; }
-  void AddBlockMoveIntra(int num = 1) { num_block_move_intra_ += num; }
-  void AddBlockMoveInter(int num = 1) { num_block_move_inter_ += num; }
+  void AddBlockMoveIntra(int num = 1);
+  void AddBlockMoveInter(int num = 1);
   void AddPageMoveIntra(int num = 1);
   void AddPageMoveInter(int num = 1);
 
   uint64_t SumLatency();
   uint64_t SumBusUtil();
 
-  unsigned int num_table_ops() const { return num_table_ops_; }  
-  unsigned int num_block_move_intra() const { return num_block_move_intra_; }  
-  unsigned int num_block_move_inter() const { return num_block_move_inter_; }  
+  unsigned int num_table_ops() const { return num_table_ops_; }
+  uint64_t bytes_intra_channel() const { return bytes_intra_channel_; }
+  uint64_t bytes_inter_channel() const { return bytes_inter_channel_; }
 
   void set_op_latency(int64_t lat) { op_latency_ = lat; }
-  void set_bus_util_intra_(int64_t util) { bus_util_intra_ = util; }
-  void set_bus_util_inter_(int64_t util) { bus_util_inter_ = util; }
+  void set_exclude_intra(bool b = true) { exclude_intra_ = b; }
 
   static Profiler Null;
   static Profiler Overlap;
@@ -35,39 +34,42 @@ class Profiler {
  private:
   const int block_bits_;
   const int page_bits_;
-  const int page_blocks_;
 
   unsigned int num_table_ops_;
-  unsigned int num_block_move_intra_;
-  unsigned int num_block_move_inter_;
+  uint64_t bytes_intra_channel_;
+  uint64_t bytes_inter_channel_;
 
   int64_t op_latency_;
-  int64_t bus_util_intra_;
-  int64_t bus_util_inter_;
+  bool exclude_intra_;
 };
 
 inline Profiler::Profiler(int block_bits, int page_bits) :
     block_bits_(block_bits), page_bits_(page_bits),
-    page_blocks_(1 << (page_bits - block_bits)),
-    num_table_ops_(0), num_block_move_intra_(0), num_block_move_inter_(0) {
+    num_table_ops_(0), bytes_intra_channel_(0), bytes_inter_channel_(0) {
   op_latency_ = -1;
-  bus_util_intra_ = -1;
-  bus_util_inter_ = -1;
+  exclude_intra_ = false;
 }
 
 inline Profiler::Profiler(const Profiler& p) :
     Profiler(p.block_bits_, p.page_bits_) {
   op_latency_ = p.op_latency_;
-  bus_util_intra_ = p.bus_util_intra_;
-  bus_util_inter_ = p.bus_util_inter_;
+  exclude_intra_ = p.exclude_intra_;
+}
+
+inline void Profiler::AddBlockMoveIntra(int num) {
+  bytes_intra_channel_ += num << block_bits_;
+}
+
+inline void Profiler::AddBlockMoveInter(int num) {
+  bytes_inter_channel_ += num << block_bits_;
 }
 
 inline void Profiler::AddPageMoveIntra(int num) {
-  num_block_move_intra_ += num * page_blocks_;
+  bytes_intra_channel_ += num << page_bits_;
 }
 
 inline void Profiler::AddPageMoveInter(int num) {
-  num_block_move_inter_ += num * page_blocks_;
+  bytes_inter_channel_ += num << page_bits_;
 }
 
 inline uint64_t Profiler::SumLatency() {
@@ -76,9 +78,7 @@ inline uint64_t Profiler::SumLatency() {
 }
 
 inline uint64_t Profiler::SumBusUtil() {
-  assert(bus_util_intra_ >= 0 && bus_util_inter_ >= 0);
-  return bus_util_intra_ * num_block_move_intra_ +
-      bus_util_inter_ * num_block_move_inter_;
+  return (exclude_intra_ ? 0 : bytes_intra_channel_) + bytes_inter_channel_;
 }
 
 #endif // SEXAIN_PROFILER_H_
