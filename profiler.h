8f71ddfcaba4980906198390b9ebef57450347b4
Jinglei Ren <jinglei.ren@gmail.com>
2015-12-08 13:16:56 +0800

    Format to gem5 coding style: profier.h/cc

--- profiler.h
+++ profiler.h
@@ -1,117 +1,195 @@
-// profiler.h
-// Copyright (c) 2014 Jinglei Ren <jinglei@ren.systems>
+/*
+ * Copyright (c) 2014 Jinglei Ren <jinglei.ren@persper.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met: redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer;
+ * redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution;
+ * neither the name of the copyright holders nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
-#ifndef SEXAIN_PROFILER_H_
-#define SEXAIN_PROFILER_H_
+#ifndef __THYNVM_PROFILER_HH__
+#define __THYNVM_PROFILER_HH__
 
-#include <cstdint>
 #include <cassert>
+#include <cstdint>
 
-class Profiler {
- public:
-  Profiler(int block_bits, int page_bits);
-  Profiler(const Profiler& p);
+namespace thynvm {
 
-  void AddTableOp(int num = 1);
-  void AddBufferOp(int num = 1);
-  void AddLatency(int lat);
-  void AddBlockMoveIntra(int num = 1);
-  void AddBlockMoveInter(int num = 1);
-  void AddPageMoveIntra(int num = 1);
-  void AddPageMoveInter(int num = 1);
+class Profiler
+{
+  public:
+    Profiler();
+    Profiler(const Profiler& p);
 
-  uint64_t SumLatency();
-  uint64_t SumBusUtil(bool exc_intra = false);
+    void setOpLatency(uint64_t latency);
+    void setBlockTraffic(uint64_t bytes);
+    void setPageTraffic(uint64_t bytes);
 
-  uint64_t num_table_ops() const { return num_table_ops_; }
-  uint64_t num_buffer_ops() const { return num_buffer_ops_; }
-  uint64_t bytes_intra_channel() const { return bytes_intra_channel_; }
-  uint64_t bytes_inter_channel() const { return bytes_inter_channel_; }
-  void set_ignore_latency();
-  void clear_ignore_latency();
+    void addLatency(uint64_t lat);
+    void addTableOp(int num = 1);
+    void addBufferOp(int num = 1);
 
-  void set_op_latency(int64_t lat) { op_latency_ = lat; }
+    void addBlockIntraChannel(int num = 1);
+    void addBlockInterChannel(int num = 1);
+    void addPageIntraChannel(int num = 1);
+    void addPageInterChannel(int num = 1);
 
-  static Profiler Null;
-  static Profiler Overlap;
+    uint64_t sumLatency();
+    uint64_t sumTraffic(bool excluding_intra = false);
 
- private:
-  const int block_bits_;
-  const int page_bits_;
+    void setIgnoreLatency();
+    void clearIgnoreLatency();
 
-  uint64_t num_table_ops_;
-  uint64_t num_buffer_ops_;
-  uint64_t latency_;
+    static Profiler Null;
+    static Profiler Overlap;
 
-  uint64_t bytes_intra_channel_;
-  uint64_t bytes_inter_channel_;
+  private:
+    uint64_t _op_latency;
+    uint64_t _block_bytes;
+    uint64_t _page_bytes;
 
-  int64_t op_latency_;
-  bool ignore_latency_;
+    uint64_t num_table_ops;
+    uint64_t num_buffer_ops;
+    uint64_t latency;
+
+    uint64_t bytes_intra_channel;
+    uint64_t bytes_inter_channel;
+
+    bool _ignore_latency;
 };
 
-inline Profiler::Profiler(int block_bits, int page_bits) :
-    block_bits_(block_bits), page_bits_(page_bits),
-    num_table_ops_(0), num_buffer_ops_(0), latency_(0),
-    bytes_intra_channel_(0), bytes_inter_channel_(0) {
-  op_latency_ = -1;
-  ignore_latency_ = false;
+inline
+Profiler::Profiler()
+        : _op_latency(0), _block_bytes(0), _page_bytes(0),
+          num_table_ops(0), num_buffer_ops(0),
+          latency(0), bytes_intra_channel(0), bytes_inter_channel(0)
+{
+    _ignore_latency = false;
 }
 
-inline Profiler::Profiler(const Profiler& p) :
-    Profiler(p.block_bits_, p.page_bits_) {
-  op_latency_ = p.op_latency_;
+inline
+Profiler::Profiler(const Profiler& p)
+        : Profiler()
+{
+    _op_latency = p._op_latency;
+    _block_bytes = p._block_bytes;
+    _page_bytes = p._page_bytes;
+    _ignore_latency = p._ignore_latency;
 }
 
-inline void Profiler::set_ignore_latency() {
-  assert(!ignore_latency_);
-  ignore_latency_ = true;
+inline void
+Profiler::setOpLatency(uint64_t latency)
+{
+    _op_latency = latency;
 }
 
-inline void Profiler::clear_ignore_latency() {
-  assert(ignore_latency_);
-  ignore_latency_ = false;
+inline void
+Profiler::setBlockTraffic(uint64_t bytes)
+{
+    _block_bytes = bytes;
 }
 
-inline void Profiler::AddTableOp(int num) {
-  if (ignore_latency_) return;
-  num_table_ops_ += num;
+inline void
+Profiler::setPageTraffic(uint64_t bytes)
+{
+    _page_bytes = bytes;
 }
 
-inline void Profiler::AddBufferOp(int num) {
-  if (ignore_latency_) return;
-  num_buffer_ops_ += num;
+inline void
+Profiler::setIgnoreLatency()
+{
+    assert(!_ignore_latency);
+    _ignore_latency = true;
 }
 
-inline void Profiler::AddLatency(int lat) {
-  assert(lat > 0 && !ignore_latency_);
-  latency_ += lat;
+inline void
+Profiler::clearIgnoreLatency()
+{
+    assert(_ignore_latency);
+    _ignore_latency = false;
 }
 
-inline void Profiler::AddBlockMoveIntra(int num) {
-  bytes_intra_channel_ += num << block_bits_;
+inline void
+Profiler::addTableOp(int num)
+{
+    if (!_ignore_latency) {
+        num_table_ops += num;
+    }
 }
 
-inline void Profiler::AddBlockMoveInter(int num) {
-  bytes_inter_channel_ += num << block_bits_;
+inline void
+Profiler::addBufferOp(int num)
+{
+    if (!_ignore_latency) {
+        num_buffer_ops += num;
+    }
 }
 
-inline void Profiler::AddPageMoveIntra(int num) {
-  bytes_intra_channel_ += num << page_bits_;
+inline void
+Profiler::addLatency(uint64_t lat)
+{
+    if (!_ignore_latency) {
+        latency += lat;
+    }
 }
 
-inline void Profiler::AddPageMoveInter(int num) {
-  bytes_inter_channel_ += num << page_bits_;
+inline void
+Profiler::addBlockIntraChannel(int num)
+{
+    bytes_intra_channel += num * _block_bytes;
 }
 
-inline uint64_t Profiler::SumLatency() {
-  assert(op_latency_ >= 0);
-  assert(!ignore_latency_);
-  return latency_ + op_latency_ * (num_table_ops_ + num_buffer_ops_);
+inline void
+Profiler::addBlockInterChannel(int num)
+{
+    bytes_inter_channel += num * _block_bytes;
 }
 
-inline uint64_t Profiler::SumBusUtil(bool exc_intra) {
-  return (exc_intra ? 0 : bytes_intra_channel_) + bytes_inter_channel_;
+inline void
+Profiler::addPageIntraChannel(int num)
+{
+    bytes_intra_channel += num * _page_bytes;
 }
 
-#endif // SEXAIN_PROFILER_H_
+inline void
+Profiler::addPageInterChannel(int num)
+{
+    bytes_inter_channel += num * _page_bytes;
+}
+
+inline uint64_t
+Profiler::sumLatency()
+{
+    assert(_op_latency >= 0);
+    assert(!_ignore_latency);
+    return latency + _op_latency * (num_table_ops + num_buffer_ops);
+}
+
+inline uint64_t
+Profiler::sumTraffic(bool excluding_intra)
+{
+    return (excluding_intra ? 0 : bytes_intra_channel) + bytes_inter_channel;
+}
+
+}  // namespace thynvm
+
+#endif  // __THYNVM_PROFILER_HH__
